41d7383ccd86b690aee030471cb23191fbc8ad1b
Eva G Cagigas <evarteca@gmail.com>
2022-03-18 13:28:23 +0100

    rover api services ready

--- routes/gallery.routes.js
+++ routes/gallery.routes.js
@@ -1,14 +1,19 @@
+//!IMPORTS, VARIABLES & REQUIRES:
 const router = require("express").Router();
+const axios = require("axios");
+const {getCuriosityGalleryService} = require("../services/gallery.services")
 
-//IMAGE GALLERY ROUTE ("/image-gallery"):
+//ROUTE TO GET ALL IMAGES GALLERY ("/image-gallery"):
 
 router.get("/", async (req, res, next)=>{
-    try {
-
+    try{       
+        const response = await getCuriosityGalleryService()        
+        console.log(response.data.photos)
+        res.json(response.data.photos)
     }
     catch(err){
-        
-    }  
+        next(err)        
+    }
 })
 
 
--- services/gallery.services.js
+++ services/gallery.services.js
@@ -1,7 +1,21 @@
 //!IMPORTS:
-import axios from "axios";
+const axios = require("axios");
 
 //!FUNCTIONS:
+const service = axios.create({
+    baseURL: process.env.API_ROVER_URL
+})
 
+const getCuriosityGalleryService = () => {
+    return service.get("/photos", {
+        params:{
+            api_key: process.env.API_KEY,
+            sol: 1000
+        }
+    })
+}
 
 //!EXPORT FUNCTIONS:
+
+module.exports = {getCuriosityGalleryService}
+
