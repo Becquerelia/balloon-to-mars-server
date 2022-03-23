09c2d438f38749ecd438c6bd346ef819ad56e789
Eva G Cagigas <evarteca@gmail.com>
2022-03-23 21:37:50 +0100

    delete & edit user routes created

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -27,6 +27,31 @@ router.get("/my-bookings", async (req, res, next)=>{
     }  
 })
 
+// DELETE-ROUTE TO DELETE USER ("/profile/delete"):
+router.delete("/delete", async (req, res, next)=>{
+    const userId = req.payload._id
+    try {
+        await UserModel.findByIdAndDelete(userId);
+        res.json("Deleted user")        
+    }
+    catch(err){
+        next(err)        
+    }  
+})
+
+// PATCH-ROUTE TO EDIT USER ("/profile/edit"):
+router.patch("/edit", async (req, res, next)=>{
+    const userId = req.payload._id
+    const {email, password, city, country} = req.body;
+    try {
+        await UserModel.findByIdAndUpdate(userId, {email, password, city, country});
+        res.json("Updated user")        
+    }
+    catch(err){
+        next(err)        
+    }  
+})
+
 // GET-ROUTE TO ADMIN PROFILE ("/profile/admin"):
 router.get("/admin", async (req, res, next)=>{
     const userId = req.payload._id
