b37e4210eb463e5ae2c3c8c82d7db31ea77895be
Eva G Cagigas <evarteca@gmail.com>
2022-03-24 20:10:54 +0100

    avatar user created (fixed route)

--- routes/uploaderpic.routes.js
+++ routes/uploaderpic.routes.js
@@ -8,7 +8,7 @@ router.post("/", fileUploader.single("imageUrl"), (req, res, next)=>{
         next(new Error("No file uploaded!"))
         return;        
     }
-    res.json({imageUrl: req.file.path})
+    res.json({fileUrl: req.file.path})
 })
 
 module.exports = router;
